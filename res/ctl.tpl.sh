0dfccde0f5495b724246ac3b44901c5e20a4c3f9
bingoohuang <bingoo.huang@gmail.com>
2019-06-19 15:34:38 +0800

    refactor ctl

--- res/ctl.tpl.sh
+++ res/ctl.tpl.sh
@@ -1,77 +1,94 @@
 #!/bin/bash
 
 export GIN_MODE=release
-
 mkdir -p var
+
+# set -x #echo on
 
 app={{.BinName}}
 pidFile=var/pid
 
 function check_pid() {
-    if [[ -f ${pidFile} ]];then
-        pid=`cat ${pidFile}`
-        if [[ -n ${pid} ]]; then
-            running=`ps -p ${pid}|grep -v "PID TTY" |wc -l`
-            return ${running}
-        fi
+  if [[ -f ${pidFile} ]]; then
+    local pid=$(cat ${pidFile})
+    if [[ -n ${pid} ]]; then
+      if [[ $(ps -p "${pid}" | wc -l) ]]; then
+        echo "${pid}"
+        return 1
+      fi
     fi
-    return 0
+  fi
+
+  # remove prefix ./
+  local pureAppName=${app#"./"}
+  local running=$(pgrep -l $pureAppName | awk '{print $1}')
+  if [[ -n ${running} ]]; then
+    echo "${running}" >${pidFile}
+    echo "${running}"
+    return 1
+  fi
+
+  echo "0"
+  return 0
 }
 
 function start() {
-    check_pid
-    running=$?
-    if [[ ${running} -gt 0 ]];then
-        echo -n "$app now is running already, pid="
-        cat ${pidFile}
-        return 1
-    fi
+  local pid=$(check_pid)
+  if [[ ${pid} -gt 0 ]]; then
+    echo -n "$app now is running already, pid=$pid"
+    return 1
+  fi
 
-    nohup ${app} {{.BinArgs}} >> ./nohup.out 2>&1 &
-    sleep 1
-    running=`ps -p $! | grep -v "PID TTY" | wc -l`
-    if [[ ${running} -gt 0 ]];then
-        echo $! > ${pidFile}
-        echo "$app started..., pid=$!"
-    else
-        echo "$app failed to start."
-        return 1
-    fi
+  nohup ${app} >>./nohup.out 2>&1 &
+  sleep 1
+  local running=$(ps -p $! | wc -l)
+  if [[ ${running} -gt 0 ]]; then
+    echo $! >${pidFile}
+    echo "$app started..., pid=$!"
+    return 0
+  else
+    echo "$app failed to start."
+    return 1
+  fi
 }
 
 function stop() {
-    pid=`cat ${pidFile}`
-    if [[ $? -eq 0 ]]; then
-        kill ${pid}
-        rm -f ${pidFile}
-    fi
-    echo "${app} ${pid} stopped..."
+  local pid=$(check_pid)
+  if [[ $pid -gt 0 ]]; then
+    kill "${pid}"
+    rm -f ${pidFile}
+  fi
+  echo "${app} ${pid} stopped..."
 }
 
 function status() {
-    check_pid
-    running=$?
-    if [[ ${running} -gt 0 ]];then
-        echo "started, pid=`cat ${pidFile}`"
-    else
-        echo "stopped!"
-    fi
+  local pid=$(check_pid)
+  if [[ ${pid} -gt 0 ]]; then
+    echo "started, pid=$pid"
+  else
+    echo "stopped!"
+  fi
 }
 
+function tailLog() {
+  local ba=$(basename ${app})
+  local logfile="var/${ba}.log"
+  local realfile=$(readlink "${logfile}")
+  tail -f "$realfile"
+}
 
-if [[ "$1" == "stop" ]];then
-    stop
-elif [[ "$1" == "start" ]];then
-    start
-elif [[ "$1" == "restart" ]];then
-    stop
-    sleep 1
-    start
-elif [[ "$1" == "status" ]];then
-    status
-elif [[ "$1" == "tail" ]];then
-    ba=`basename ${app}`
-    tail -f `ls -l var/${ba}.log | awk '{print $NF}'`
+if [[ "$1" == "stop" ]]; then
+  stop
+elif [[ "$1" == "start" ]]; then
+  start
+elif [[ "$1" == "restart" ]]; then
+  stop
+  sleep 1
+  start
+elif [[ "$1" == "status" ]]; then
+  status
+elif [[ "$1" == "tail" ]]; then
+  tailLog
 else
-    echo "$0 start|stop|restart|status|tail"
+  echo "$0 start|stop|restart|status|tail"
 fi
